15b030af03f55ddb8238b08f2861c2d6a96c0dc1
Nicholas Law <nick@nicholaslaw.com.au>
2020-03-14 15:51:34 +1100

    Add private VPC NAT Gateways

--- terraform/vpc-private.tf
+++ terraform/vpc-private.tf
@@ -51,3 +51,88 @@ resource "aws_subnet" "private_c" {
     ManagedBy  = var.managed_by
   }
 }
+
+# ==========================================
+# == PRIVATE INTERNET GATEWAY
+# ==========================================
+
+resource "aws_internet_gateway" "private_vpc_igw" {
+  vpc_id = aws_vpc.private.id
+
+  tags = {
+    Name       = "private-vpc-igw"
+    Repository = var.repository
+    ManagedBy  = var.managed_by
+  }
+}
+
+# ==========================================
+# == PRIVATE NAT GATEWAY ELASTIC IPS
+# ==========================================
+
+resource "aws_eip" "private_ngw_eip_a" {
+  vpc = true
+
+  tags = {
+    Name       = "private-ngw-eip-a"
+    Repository = var.repository
+    ManagedBy  = var.managed_by
+  }
+}
+
+resource "aws_eip" "private_ngw_eip_b" {
+  vpc = true
+
+  tags = {
+    Name       = "private-ngw-eip-b"
+    Repository = var.repository
+    ManagedBy  = var.managed_by
+  }
+}
+
+resource "aws_eip" "private_ngw_eip_c" {
+  vpc = true
+
+  tags = {
+    Name       = "private-ngw-eip-c"
+    Repository = var.repository
+    ManagedBy  = var.managed_by
+  }
+}
+
+# ==========================================
+# == PRIVATE NAT GATEWAYS
+# ==========================================
+
+resource "aws_nat_gateway" "private_ngw_a" {
+  allocation_id = aws_eip.private_ngw_eip_a.id
+  subnet_id     = aws_subnet.private_a.id
+
+  tags = {
+    Name       = "private-ngw-a"
+    Repository = var.repository
+    ManagedBy  = var.managed_by
+  }
+}
+
+resource "aws_nat_gateway" "private_ngw_b" {
+  allocation_id = aws_eip.private_ngw_eip_b.id
+  subnet_id     = aws_subnet.private_b.id
+
+  tags = {
+    Name       = "private-ngw-b"
+    Repository = var.repository
+    ManagedBy  = var.managed_by
+  }
+}
+
+resource "aws_nat_gateway" "private_ngw_c" {
+  allocation_id = aws_eip.private_ngw_eip_c.id
+  subnet_id     = aws_subnet.private_c.id
+
+  tags = {
+    Name       = "private-ngw-c"
+    Repository = var.repository
+    ManagedBy  = var.managed_by
+  }
+}
